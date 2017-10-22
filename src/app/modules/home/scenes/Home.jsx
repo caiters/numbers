e5e7617ca69fa2e0b7558a4450633bb009ca9955
Caitlin <ayashi@valefor.com>
2017-10-22 12:38:40 -0700

    removing nav

--- src/app/modules/home/scenes/Home.jsx
+++ src/app/modules/home/scenes/Home.jsx
@@ -1,12 +1,12 @@
-import React, { Component, PropTypes } from 'react'
-import { connect } from 'react-redux'
-import reactJsLogo from '../../../resources/assets/images/react.svg'
-import './Home.scss'
+import React, { Component, PropTypes } from "react";
+import { connect } from "react-redux";
+import reactJsLogo from "../../../resources/assets/images/react.svg";
+import "./Home.scss";
 
 class Home extends Component {
   constructor(props) {
-    super(props)
-    this.state = {}
+    super(props);
+    this.state = {};
   }
 
   render() {
@@ -14,36 +14,27 @@ class Home extends Component {
       <div className="home">
         <div className="home__content">
           <a href="#" className="home__logo-link">
-            <img className="home__logo-img" src={reactJsLogo} alt="React Logo" />
+            <img
+              className="home__logo-img"
+              src={reactJsLogo}
+              alt="React Logo"
+            />
           </a>
-          <h4 className="home__heading">React-Redux Boilerplate</h4>
-          <nav className="home__nav">
-            <ul className="home__nav-list">
-              <li className="home__nav-list-item">
-                <a href="https://github.com/david-babunashvili/React-Redux-Boilerplate#readme" target="_blank" rel="noopener noreferrer" className="home__nav-list-link">DOCUMENTATION</a>
-              </li>
-              <li className="home__nav-list-item">
-                <a href="https://github.com/david-babunashvili/React-Redux-Boilerplate" target="_blank" rel="noopener noreferrer" className="home__nav-list-link">GITHUB</a>
-              </li>
-              <li className="home__nav-list-item">
-                <a href="https://github.com/david-babunashvili/React-Redux-Boilerplate/issues" target="_blank" rel="noopener noreferrer" className="home__nav-list-link">BUGS</a>
-              </li>
-            </ul>
-          </nav>
+          <h1 className="home__heading">Numbers Clicky</h1>
         </div>
       </div>
-    )
+    );
   }
 }
 
-Home.propTypes = {}
+Home.propTypes = {};
 
 function mapStateToProps(state, ownProps) {
   return {
-    ownProps,
-  }
+    ownProps
+  };
 }
 
-const mapDispatchToProps = dispatch => ({})
+const mapDispatchToProps = dispatch => ({});
 
-export default connect(mapStateToProps, mapDispatchToProps)(Home)
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
