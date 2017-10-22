1970da4d74e50853fd45ad34185aafd7d91e864b
Caitlin <ayashi@valefor.com>
2017-10-22 16:10:15 -0700

    moving timer stuff

---
+++ src/app/modules/timer/timerReducer.js
@@ -0,0 +1,17 @@
+import { NEW_GAME, SELECT_NUMBER } from "./gameBoardActions";
+import { create, selectNumber } from "./gameBoard";
+
+export default (gameBoard = create(), action) => {
+  switch (action.type) {
+    case NEW_GAME:
+      return Object.assign({}, gameBoard, create());
+    case SELECT_NUMBER:
+      return Object.assign(
+        {},
+        gameBoard,
+        selectNumber(action.index, gameBoard)
+      );
+    default:
+      return gameBoard;
+  }
+};
