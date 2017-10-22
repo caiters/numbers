1881b3195310e1b95e9cc3fe0cb540810d1eef10
Christopher Giroir <kelsin@valefor.com>
2017-10-22 13:36:20 -0700

    Two actions added

--- src/app/modules/gameboard/gameBoard.js
+++ src/app/modules/gameboard/gameBoard.js
@@ -1,7 +1,44 @@
+import * as shuffle from 'shuffle-array'
+
+const NUMBER_OF_BOXES = 10
+
 const create = () => {
+  const shuffled = shuffle(Array.from(new Array(NUMBER_OF_BOXES),
+                                      (val, index) => index + 1))
+  const numbers = shuffled.map(n => ({
+    number: n,
+    selected: false,
+  }))
+
+  const encoded = Buffer.from(shuffled).toString('base64')
+
+  return {
+    encoded,
+    lastSelected: undefined,
+    numbers,
+  }
 }
 
 const selectNumber = (index, gameBoard) => {
+  let lastSelected = gameBoard.lastSelected
+
+  const numbers = gameBoard.numbers.map((n, i) => {
+    if (i === index) {
+      // This is the number we are selecting!
+      lastSelected = n.number
+      return {
+        number: n.number,
+        selected: true,
+      }
+    }
+
+    return Object.assign({}, n)
+  })
+
+  return Object.assign({}, gameBoard, {
+    lastSelected,
+    numbers,
+  })
 }
 
 export { create, selectNumber }
