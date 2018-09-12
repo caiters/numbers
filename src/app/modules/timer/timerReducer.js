4259b6a79040d8e1665061cec7d8fcd2b748d5e3
Caitlin <ayashi@valefor.com>
2018-09-11 19:37:21 -0700

    I forgot to commit this before

--- src/app/modules/timer/timerReducer.js
+++ src/app/modules/timer/timerReducer.js
@@ -1,14 +1,16 @@
 import { START_TIMER, STOP_TIMER, RESET_TIMER } from "./timerActions";
+import { END_GAME } from "../gameboard/gameBoardActions";
 import { create, selectNumber } from "./gameBoard";
 
 export default (timer = {}, action) => {
   switch (action.type) {
     case START_TIMER:
       return Object.assign({}, timer, start());
+    case END_GAME:
     case STOP_TIMER:
       return Object.assign({}, timer, stop());
     case RESET_TIMER:
-        return Object.assign({}, timer, reset());
+      return Object.assign({}, timer, reset());
     default:
       return timer;
   }
